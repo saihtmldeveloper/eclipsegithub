cfda4d2d58ee428b2e57353de4d145fe48207237
sai kishore <psaikishore12@gmail.com>
2019-03-12 11:17:56 +0530

    initial commit

---
+++ eclipsegithub/src/eclipsegithub/main.java
@@ -0,0 +1,7 @@
+package eclipsegithub;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println("main method added");
+	}
+}
---
+++ eclipsegithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author 91824
+ *
+ */
+module eclipsegithub {
+}
